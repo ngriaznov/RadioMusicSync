a3153c02b5181344edc07ff9cbd48d9a9c111157
ngriaznov <nikita.griaznov@gmail.com>
2016-10-18 21:43:28 +0300

    Code Reformatting

--- RadioMusic/RadioMusic.ino
+++ RadioMusic/RadioMusic.ino
@@ -1,10 +1,5 @@
-//#include <StandardCplusplus.h>
-//#include <system_configuration.h>
-//#include <unwind-cxx.h>
-//#include <utility.h>
-
 /*
-  RADIO MUSIC
+  RADIO MUSIC SYNC, based on:
   https://github.com/TomWhitwell/RadioMusic
 
   Audio out: Onboard DAC, teensy3.1 pin A14/DAC
@@ -39,123 +34,118 @@
 #include <SD.h>
 #include <Wire.h>
 
-// OPTIONS TO READ FROM THE SD CARD, WITH DEFAULT VALUES
-boolean MUTE = false;  // Softens clicks when changing channel / position, at
-                       // cost of speed. Fade speed is set by DECLICK
-int DECLICK = 5;       // milliseconds of fade in/out on switching
-boolean ShowMeter = true;  // Does the VU meter appear?
-int meterHIDE =
-    2000;  // how long to show the meter after bank change in Milliseconds
-boolean ChanPotImmediate = true;  // Settings for Pot / CV response.
-boolean ChanCVImmediate = true;  // TRUE means it jumps directly when you move or change.
-boolean StartPotImmediate = false;  // FALSE means it only has an effect when RESET is pushed or triggered
-boolean StartCVImmediate = false;
-int StartCVDivider = 2;  // Changes sensitivity of Start control. 1 = most sensitive, 512 = lest sensitive (i.e only two points)
-boolean Looping = true;  // When a file finishes, start again from the beginning
-int currentTimePosition = 0;
-
-File settingsFile;
-
-// GUItool: begin automatically generated code
-AudioMixer4 mixer;
-AudioPlaySdRaw playRaw1;  // xy=131,81
-AudioPlaySdRaw playRaw2;  // xy=131,81
-AudioEffectFade fade1;    // xy=257,169
-AudioEffectFade fade2;    // xy=257,169
-AudioAnalyzePeak peak1;   // xy=317,123
-AudioOutputAnalog dac1;   // xy=334,98
-AudioConnection patchCord1(playRaw1, fade1);
-AudioConnection patchCord4(playRaw2, fade2);
-AudioConnection patchCord7(mixer, dac1);
-AudioConnection patchCord2(fade1, 0, mixer, 0);
-AudioConnection patchCord6(fade2, 0, mixer, 1);
-AudioConnection patchCord3(mixer, peak1);
-// GUItool: end automatically generated code
-
 // REBOOT CODES
-#define RESTART_ADDR 0xE000ED0C
-#define READ_RESTART() (*(volatile uint32_t *)RESTART_ADDR)
-#define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
+#define                     RESTART_ADDR 0xE000ED0C
+#define                     READ_RESTART() (*(volatile uint32_t *)RESTART_ADDR)
+#define                     WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
 
 // SETUP VARS TO STORE DETAILS OF FILES ON THE SD CARD
-#define MAX_FILES 75
-#define BANKS 16
-int ACTIVE_BANKS;
-String FILE_TYPE = "RAW";
-String FILE_NAMES[BANKS][MAX_FILES];
-String FILE_DIRECTORIES[BANKS][MAX_FILES];
-unsigned long FILE_SIZES[BANKS][MAX_FILES];
-int FILE_COUNT[BANKS];
-String CURRENT_DIRECTORY = "0";
-File root;
-#define BLOCK_SIZE 2  // size of blocks to read - must be more than 1, performance might improve
-     // with 16?
+#define                     MAX_FILES 75
+#define                     BANKS 16
 
 // SETUP VARS TO STORE CONTROLS
-#define CHAN_POT_PIN A9  // pin for Channel pot
-#define CHAN_CV_PIN A6   // pin for Channel CV
-#define TIME_POT_PIN A7  // pin for Time pot
-#define TIME_CV_PIN A8   // pin for Time CV
-#define RESET_BUTTON 8   // Reset button
-#define RESET_LED 11     // Reset LED indicator
-#define RESET_CV 9       // Reset pulse input
-
-boolean CHAN_CHANGED = true;
-boolean RESET_CHANGED = false;
-boolean CLOCK_CHANGED = false;
-unsigned long PLAY_POSITION = 0;
-int CLOCK_SKIP = 0;
-
-Bounce resetSwitch = Bounce(RESET_BUTTON, 20);  // Bounce setup for Reset
-int PLAY_CHANNEL;
-int NEXT_CHANNEL;
-unsigned long playhead;
-char *charFilename;
-char *secondCharFilename;
+#define                     CHAN_POT_PIN A9                               // pin for Channel pot
+#define                     CHAN_CV_PIN A6                                // pin for Channel CV
+#define                     TIME_POT_PIN A7                               // pin for Time pot
+#define                     TIME_CV_PIN A8                                // pin for Time CV
+#define                     RESET_BUTTON 8                                // Reset button
+#define                     RESET_LED 11                                  // Reset LED indicator
+#define                     RESET_CV 9                                    // Reset pulse input
 
 // BANK SWITCHER SETUP
-#define BANK_BUTTON 2  // Bank Button
-#define LED0 6
-#define LED1 5
-#define LED2 4
-#define LED3 3
-Bounce bankSwitch = Bounce(BANK_BUTTON, 20);
-int PLAY_BANK = 0;
-#define BANK_SAVE 0
+#define                     BANK_BUTTON 2                                 // Bank Button
+#define                     LED0 6
+#define                     LED1 5
+#define                     LED2 4
+#define                     LED3 3
+#define                     BANK_SAVE 0
+
+#define                     peakFPS 12                                    //  FRAMERATE FOR PEAK METER
+
+#define                     FLASHTIME 10                                  // How long do LEDs flash for?
+#define                     HOLDTIME 400                                  // How many millis to hold a button to get 2ndary function?
+
+// OPTIONS TO READ FROM THE SD CARD, WITH DEFAULT VALUES
+boolean                     MUTE = false;                                 // Softens clicks when changing channel / position, at
+                                                                          // cost of speed. Fade speed is set by DECLICK
+int                         DECLICK = 2;                                  // milliseconds of fade in/out on switching
+boolean                     ShowMeter = true;                             // Does the VU meter appear?
+int                         meterHIDE = 2000;                             // how long to show the meter after bank change in Milliseconds
+boolean                     ChanPotImmediate = true;                      // Settings for Pot / CV response.
+boolean                     ChanCVImmediate = true;                       // TRUE means it jumps directly when you move or change.
+boolean                     StartPotImmediate = false;                    // FALSE means it only has an effect when RESET is pushed or triggered
+boolean                     StartCVImmediate = false;
+int                         StartCVDivider = 2;                           // Changes sensitivity of Start control. 1 = most sensitive, 512 = lest sensitive (i.e only two points)
+boolean                     Looping = true;                               // When a file finishes, start again from the beginning
+int                         currentTimePosition = 0;
+int                         BPM = 130;                                    // Base BPM for loops
+int                         skipTransition = 0;
+
+File                        settingsFile;
+
+AudioMixer4                 mixer;
+AudioPlaySdRaw              playRaw1;  
+AudioPlaySdRaw              playRaw2;  
+AudioEffectFade             fade1;    
+AudioEffectFade             fade2;
+AudioAnalyzePeak            peak1;
+AudioOutputAnalog           dac1;
+AudioConnection             patchCord1(playRaw1, fade1);
+AudioConnection             patchCord4(playRaw2, fade2);
+AudioConnection             patchCord7(mixer, dac1);
+AudioConnection             patchCord2(fade1, 0, mixer, 0);
+AudioConnection             patchCord6(fade2, 0, mixer, 1);
+AudioConnection             patchCord3(mixer, peak1);
+
+int                         ACTIVE_BANKS;
+String                      FILE_TYPE = "RAW";
+String                      FILE_NAMES[BANKS][MAX_FILES];
+String                      FILE_DIRECTORIES[BANKS][MAX_FILES];
+unsigned long               FILE_SIZES[BANKS][MAX_FILES];
+int                         FILE_COUNT[BANKS];
+String                      CURRENT_DIRECTORY = "0";
+File                        root;
+
+boolean                     CHAN_CHANGED = true;
+boolean                     RESET_CHANGED = false;
+boolean                     CLOCK_CHANGED = false;
+unsigned long               PLAY_POSITION = 0;
+int                         CLOCK_SKIP = 0;
+
+Bounce                      resetSwitch = Bounce(RESET_BUTTON, 20);       // Bounce setup for Reset
+int                         PLAY_CHANNEL;
+int                         NEXT_CHANNEL;
+unsigned long               playhead;
+char                        *targetFile;
+
+
+Bounce                      bankSwitch = Bounce(BANK_BUTTON, 20);
+int                         PLAY_BANK = 0;
 
 // CHANGE HOW INTERFACE REACTS
-int chanHyst = 3;  // how many steps to move before making a change (out of 1024
-                   // steps on a reading)
-int timHyst = 6;
+int                         chanHyst = 3;                                 // how many steps to move before making a change (out of 1024 steps on a reading)
+int                         timHyst = 6;
 
-elapsedMillis chanChanged;
-elapsedMillis timChanged;
-elapsedMicros clockPeriod;
+elapsedMillis               chanChanged;
+elapsedMillis               timChanged;
 
-int sampleAverage = 40;
-int chanPotOld;
-int chanCVOld;
-int timPotOld;
-int timCVOld;
-#define FLASHTIME 10  // How long do LEDs flash for?
-#define HOLDTIME \
-  400  // How many millis to hold a button to get 2ndary function?
-elapsedMillis showDisplay;  // elapsedMillis is a special variable in Teensy -
-                            // increments every millisecond
-int showFreq = 250;         // how many millis between serial Debug updates
-elapsedMillis resetLedTimer = 0;
-elapsedMillis bankTimer = 0;
-elapsedMillis checkI = 0;  // check interface
-int checkFreq = 10;        // how often to check the interface in Millis
+int                         sampleAverage = 40;
+int                         chanPotOld;
+int                         chanCVOld;
+int                         timPotOld;
+int                         timCVOld;
+
+elapsedMillis               showDisplay;                                  // elapsedMillis is a special variable in Teensy - increments every millisecond
+int                         showFreq = 250;                               // how many millis between serial Debug updates
+elapsedMillis               resetLedTimer = 0;
+elapsedMillis               bankTimer = 0;
+elapsedMillis               checkI = 0;                                   // check interface
+int                         checkFreq = 10;                               // how often to check the interface in Millis
 
 // CONTROL THE PEAK METER DISPLAY
-elapsedMillis meterDisplay;  // Counter to hide MeterDisplay after bank change
-elapsedMillis fps;           // COUNTER FOR PEAK METER FRAMERATE
-elapsedMicros fadeCompleted;
-elapsedMillis resetTimer;
-bool fadeSwitch = false;
-
-#define peakFPS 12  //  FRAMERATE FOR PEAK METER
+elapsedMillis               meterDisplay;                                 // Counter to hide MeterDisplay after bank change
+elapsedMillis               fps;                                          // COUNTER FOR PEAK METER FRAMERATE
+bool                        fadeSwitch = false;
 
 void setup() {
   // PINS FOR BANK SWITCH AND LEDS
@@ -220,57 +210,55 @@ void setup() {
   // Add an interrupt on the RESET_CV pin to catch rising edges
   attachInterrupt(RESET_CV, resetcv, RISING);
   attachInterrupt(CHAN_CV_PIN, clockrecieve, RISING);
+
+  skipTransition = round(44.1 * 30000/BPM); // ((60000/BPM/4)*2 * (44100/1000))
 }
 
 // Called by interrupt on rising edge, for RESET_CV pin
-void resetcv() { RESET_CHANGED = true; }
+void resetcv() { 
+  RESET_CHANGED = true; 
+}
 
 // Called when station recieves clock
-void clockrecieve() { CLOCK_CHANGED = true; }
-
-////////////////////////////////////////////////////
-///////////////MAIN LOOP//////////////////////////
-////////////////////////////////////////////////////
+void clockrecieve() { 
+  CLOCK_CHANGED = true; 
+}
 
 void loop() {
-  //////////////////////////////////////////
-  // IF FILE ENDS, RESTART FROM THE BEGINNING
-  //////////////////////////////////////////
-
   if (!playRaw1.isPlaying() && Looping) {
-    charFilename = buildPath(PLAY_BANK, PLAY_CHANNEL);
-    playRaw1.playFrom(charFilename, 0);  // change audio
-    playRaw2.playFrom(charFilename, 0);  // change audio
-    resetLedTimer = 0;                   // turn on Reset LED
+    targetFile = buildPath(PLAY_BANK, PLAY_CHANNEL);
+    playRaw1.playFrom(targetFile, 0);
   }
+
+  if (!playRaw2.isPlaying() && Looping) {
+    targetFile = buildPath(PLAY_BANK, PLAY_CHANNEL);
+    playRaw2.playFrom(targetFile, 0);       
+  }
+
+  resetLedTimer = 0;  
 
   if (playRaw1.failed) {
     reBoot();
   }
 
-  //////////////////////////////////////////
-  ////////REACT TO ANY CHANGES /////////////
-  //////////////////////////////////////////
-
   if (CHAN_CHANGED) {
-    charFilename = buildPath(PLAY_BANK, NEXT_CHANNEL);
-
+    targetFile = buildPath(PLAY_BANK, NEXT_CHANNEL);
     PLAY_CHANNEL = NEXT_CHANNEL;
 
     if (RESET_CHANGED == false && Looping)
       playhead = playRaw1.fileOffset();  // Carry on from previous position,
     
-    // unless reset pressed or time selected
-    playhead = (playhead / 16) * 16;  // scale playhead to 16 step chunks
+      // unless reset pressed or time selected
+      playhead = (playhead / 16) * 16;  // scale playhead to 16 step chunks
 
-    playRaw1.playFrom(charFilename, playhead);        // change audio
-    playRaw2.playFrom(charFilename, playhead);  // change audio
+      playRaw1.playFrom(targetFile, playhead);        // change audio
+      playRaw2.playFrom(targetFile, playhead);        // change audio
 
-    PLAY_POSITION = playhead;
+      PLAY_POSITION = playhead;
 
-    ledWrite(PLAY_BANK);
-    CHAN_CHANGED = false;
-    resetLedTimer = 0;  // turn on Reset LED
+      ledWrite(PLAY_BANK);
+      CHAN_CHANGED = false;
+      resetLedTimer = 0;  // turn on Reset LED
   }
 
   if (RESET_CHANGED){
@@ -278,30 +266,27 @@ void loop() {
     RESET_CHANGED = false;
   }
   
-  if (CLOCK_CHANGED || RESET_CHANGED) { // reset timer must be more than 100 ms to count in on new clock, depends on bpm
-    charFilename = buildPath(PLAY_BANK, PLAY_CHANNEL);
+  if (CLOCK_CHANGED || RESET_CHANGED) { 
+    targetFile = buildPath(PLAY_BANK, PLAY_CHANNEL);
     PLAY_POSITION = (PLAY_POSITION / 16) * 16; 
+    
     if (fadeSwitch){            
-      playRaw2.playFrom(charFilename, PLAY_POSITION);
+      playRaw2.playFrom(targetFile, PLAY_POSITION);
       fade1.fadeOut(10);
       fade2.fadeIn(10);          
     }
     else {
-      playRaw1.playFrom(charFilename, PLAY_POSITION);
+      playRaw1.playFrom(targetFile, PLAY_POSITION);
       fade1.fadeIn(10);
       fade2.fadeOut(10);
     }
 
     fadeSwitch = !fadeSwitch;
     
-    PLAY_POSITION = PLAY_POSITION + 10176; // depends on bpm
+    PLAY_POSITION = PLAY_POSITION + skipTransition; 
     CLOCK_CHANGED = false;
     RESET_CHANGED = false;
   }
-
-  //////////////////////////////////////////
-  // CHECK INTERFACE  & UPDATE DISPLAYS/////
-  //////////////////////////////////////////
 
   if (checkI >= checkFreq) {
     checkInterface();
@@ -309,8 +294,6 @@ void loop() {
   }
 
   if (showDisplay > showFreq) {
-    //    playDisplay();
-    whatsPlaying();
     showDisplay = 0;
   }
 
